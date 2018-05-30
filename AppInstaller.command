ccd530bb6e531744b6749255d04bc7a25e8565d5
GaryGao <garygaochinese@gmail.com>
2018-05-30 11:54:58 +0800

    201805301154

---
+++ AppInstaller.command
@@ -0,0 +1,2 @@
+brew install gdbm libyaml mackup mas maven openssl python python@2 readline ruby sqlite telnet xz zookeeper zsh zsh-completions 
+brew cask install alfred baidunetdisk cheatsheet evernote google-chrome iina intellij-idea java8 jietu launchrocket neteasemusic pycharm qq robo-3t shadowsocksx-ng sourcetree squirrel sublime-text switchhosts the-unarchiver thunder 
---
+++ AppListAutoBackup.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+# 备份
+
+AppList_Folder="$HOME/Documents/AppList"
+Applications_Folder_List="$AppList_Folder/Applications_Folder_List.txt"
+MAS_List="$AppList_Folder/MAS_List.txt"
+Brew_List="$AppList_Folder/Brew_List.txt"
+BrewCask_List="$AppList_Folder/BrewCask_List.txt"
+AppInstaller="$AppList_Folder/AppInstaller.command"
+
+AppList_Folder_GIT="$AppList_Folder/.git"
+
+if [ ! -d $AppList_Folder ]; then
+  mkdir $AppList_Folder
+fi
+
+# All Apps
+ls -lh /Applications > $Applications_Folder_List
+
+# MAS Apps
+mas list > $MAS_List
+
+# brew Apps
+brew list > $Brew_List
+
+# brew cask Apps
+brew cask list > $BrewCask_List
+
+# 生成 MAS_List 安装命令
+cat $MAS_List | sed "s/(.*)//g" | sed -Ee 's/([0-9]+) (.+)/mas install \1 #\2/g' > $AppInstaller
+
+# 生成 Brew_List 安装命令
+echo "brew install $(cat $Brew_List | tr '\n' ' ')" >> $AppInstaller
+
+# 生成 BrewCask_List 安装命令
+echo "brew cask install $(cat $BrewCask_List | tr '\n' ' ')" >> $AppInstaller
+
+# 赋予权限
+if [ ! -x $AppInstaller ]; then
+  chmod +x $AppInstaller
+fi
+
+
+if [ ! -d AppList_Folder_GIT];then
+	git init AppList_Folder
+fi	
+
+
+
+
+
